7b5351f8154bd872fcaab715a5a4d67dccdcbbdb
Harshkhosla <harshkhosla9945@gmail.com>
2024-07-10 17:24:36 +0800

    sd

--- index.js
+++ index.js
@@ -148,5 +148,5 @@ server.listen(3005, () => {
 
 const PORT = process.env.PORT || 5100;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running onxsdcsdv port ${PORT}`);
 });
