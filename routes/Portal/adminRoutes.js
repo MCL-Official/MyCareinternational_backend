024b7f745ea8af6bd72e3e3be6bb2da741bf0b9c
skyi9 <88197965+skyi9@users.noreply.github.com>
2025-01-21 06:31:12 +0530

    contact form new api using outlook

--- routes/Portal/adminRoutes.js
+++ routes/Portal/adminRoutes.js
@@ -11,9 +11,27 @@ const transporter = nodemailer.createTransport({
     },
 });
 
+const transporter2 = nodemailer.createTransport({
+    service: "Outlook365",
+    host: 'smtp.office365.com',
+    port: 587,
+    tls: {
+        ciphers: 'SSLv3',
+        rejectUnauthorized: false,
+    },
+    secure: false,
+    auth: {
+        user: "info@mycarelabs.com",
+        pass: "lnqtrpqcktcppdfm",
+    },
+    // debug: true,  // Enable debugging
+    // logger: true, // Log information
+});
+
+
 const sendContactForm = async (formDetails) => {
     const mailOptions = {
-        from: "Harshkhosla9945@gmail.com",
+        from: "info@mycarelabs.com",
         // to: "info@mycaretrading.com",
         to: "admin@mycarelabs.com",
         subject: 'Contact Form Submission',
@@ -28,8 +46,8 @@ const sendContactForm = async (formDetails) => {
     };
 
     try {
-        await transporter.sendMail(mailOptions);
-        console.log('Contact form details sent successfully');
+        await transporter2.sendMail(mailOptions);
+        console.log('Contact form details sent successfully using transporter 2');
     } catch (error) {
         console.error('Error sending contact form details:', error);
     }
@@ -52,10 +70,10 @@ router.post('/api/registerform', async (req, res) => {
 
 const sendContactFormIT = async (formDetails) => {
     const mailOptions = {
-        from: "Harshkhosla9945@gmail.com",
+        from: "info@mycarelabs.com",
         // to: "info@mycaretrading.com",
         to: "info@mycareit.com",
-        subject: 'Contact Form Submission',
+        subject: 'Contact Form Submission from My Care IT',
         text: `
             Name: ${formDetails.name}
             Email: ${formDetails.email}
@@ -68,7 +86,7 @@ const sendContactFormIT = async (formDetails) => {
     };
 
     try {
-        await transporter.sendMail(mailOptions);
+        await transporter2.sendMail(mailOptions);
         console.log('Contact form details sent successfully');
     } catch (error) {
         console.error('Error sending contact form details:', error);
@@ -84,6 +102,45 @@ router.post('/api/contact/mycareit', async (req, res) => {
 
     try {
         await sendContactFormIT({ name, email, phone, message, agreeToTerms, company, service });
+        res.status(200).json({ message: 'Contact form details sent successfully' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error sending contact form details' });
+    }
+});
+
+const sendContactFormNew = async (formDetails) => {
+    const mailOptions = {
+        from: "info@mycarelabs.com",
+        // to: "info@mycaretrading.com",
+        to: "admin@mycarelabs.com",
+        subject: 'Contact Form Submission from B2B Landing Page',
+        text: `
+            Name: ${formDetails.name}
+            Email: ${formDetails.email}
+            Message: ${formDetails.message}
+            Phone: ${formDetails.phone}
+            company: ${formDetails.company}
+            zipcode: ${formDetails.zipcode}
+        `,
+    };
+
+    try {
+        await transporter2.sendMail(mailOptions);
+        console.log('Contact form details sent successfully');
+    } catch (error) {
+        console.error('Error sending contact form details:', error);
+    }
+};
+
+router.post('/api/contact/mycarelabs', async (req, res) => {
+    const { name, email, message, phone, company, zipcode } = req.body;
+    console.log("Request Body:", req.body); // Debugging
+    if (!name || !email) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+
+    try {
+        await sendContactFormNew({ name, email, phone, zipcode, message, company });
         res.status(200).json({ message: 'Contact form details sent successfully' });
     } catch (error) {
         res.status(500).json({ error: 'Error sending contact form details' });
